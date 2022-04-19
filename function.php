37258db3e0346f252db53fcd7ee656becea2ca9f
errjak <dioadyaw@gmail.com>
2022-04-19 16:14:20 +0700

    projek UTS PAW

--- function.php
+++ function.php
@@ -1,40 +1,40 @@
 <?php
-class DBController
-{
-	private $host = "localhost";
-	private $user = "root";
-	private $password = "";
-	private $database = "tblprodut";
-	private $conn;
+// class DBController
+// {
+// 	private $host = "localhost";
+// 	private $user = "root";
+// 	private $password = "";
+// 	private $database = "tblprodut";
+// 	private $conn;
 
-	function __construct()
-	{
-		$this->conn = $this->connectDB();
-	}
+// 	function __construct()
+// 	{
+// 		$this->conn = $this->connectDB();
+// 	}
 
-	function connectDB()
-	{
-		$conn = mysqli_connect("localhost", "root", "", "tblproduct");
-		return $conn;
-	}
+// 	function connectDB()
+// 	{
+// 		$conn = mysqli_connect("localhost", "root", "", "tblproduct");
+// 		return $conn;
+// 	}
 
-	function runQuery($query)
-	{
-		$result = mysqli_query($this->conn, $query);
-		while ($row = mysqli_fetch_assoc($result)) {
-			$resultset[] = $row;
-		}
-		if (!empty($resultset))
-			return $resultset;
-	}
+// 	function runQuery($query)
+// 	{
+// 		$result = mysqli_query($this->conn, $query);
+// 		while ($row = mysqli_fetch_assoc($result)) {
+// 			$resultset[] = $row;
+// 		}
+// 		if (!empty($resultset))
+// 			return $resultset;
+// 	}
 
-	function numRows($query)
-	{
-		$result  = mysqli_query($this->conn, $query);
-		$rowcount = mysqli_num_rows($result);
-		return $rowcount;
-	}
-}
+// 	function numRows($query)
+// 	{
+// 		$result  = mysqli_query($this->conn, $query);
+// 		$rowcount = mysqli_num_rows($result);
+// 		return $rowcount;
+// 	}
+// }
 $conn = mysqli_connect("localhost", "root", "", "tblproduct");
 
 //Mengambil data dari database
